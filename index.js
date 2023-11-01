208088502d111515c7f4e7e780281b2123cf0123
spcrsankar <140748143+spcrsankar@users.noreply.github.com>
2023-11-01 23:17:57 +0530

    Update index.js for cors origin issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express=require("express");
 const app=express();
-// const cors=require("cors");
+const cors=require("cors");
 const connection=require("./db");
 const userRoutes=require("./routes/users");
 const authRoutes=require("./routes/auth");
@@ -19,7 +19,7 @@ try {
     connection();
     
     app.use(express.json());
-    // app.use(cors());
+    app.use(cors());
 
     const publicDirectory = path.join(__dirname, 'uploads');
     app.use('/uploads', express.static(publicDirectory));
